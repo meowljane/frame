8b2739716dbbe8441733e737548ed85263441a64
meowljane <69661367+meowljane@users.noreply.github.com>
2020-09-07 02:16:37 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeImg(img){
+ var mapimg = document.getElementById('map');
+ mapimg.src = img;
+}
+
+function changeImg1(obj, img){
+ var mapimg1 = document.getElementById('map1');
+ mapimg1.src = img;
+}
